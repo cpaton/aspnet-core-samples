e3ffd7e63fb72dbfe65b54a00404149c1a852b00
Craig Paton <craigpaton@gmail.com>
2018-02-28 18:34:38 +0000

    Simple API example

---
+++ simple-api/WebApi/Features/FeatureA/WidgetsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Features.FeatureA
+{
+    [Route("api/[controller]")]
+    public class WidgetsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Widget> Get()
+        {
+            return new Widget[]
+                   {
+                       new Widget("WidgetA", 100),
+                       new Widget("WidgetB", 250)
+                   };
+        }
+    }
+
+    public class Widget
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public Widget(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
